47240a2f5def05c5916e680cf409b352d256a44a
Faust <153928430+ncqq@users.noreply.github.com>
2025-06-15 23:50:34 +0800

    Update Comment.tsx

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -42,7 +42,7 @@ const Comments = () => {
       {mounted ? (
         <Giscus
           id={id}
-          repo="faust6312/astroblog"
+          repo="ncqq/astroblog"
           repoId="R_kgDONnB1Tg"
           category="Announcements"
           categoryId="DIC_kwDONnB1Ts4Cl1Oz"
