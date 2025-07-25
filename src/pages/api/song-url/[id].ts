09cc5e6b8a759c9560a2d5fdfd6bf55d78a091b6
faust6312 <joe969916@gmail.com>
2025-07-25 23:38:46 +0800

    try to fix music play bug again --vercel bug

--- src/pages/api/song-url/[id].ts
+++ src/pages/api/song-url/[id].ts
@@ -1,5 +1,6 @@
 import type { APIRoute } from 'astro'
 import { getSongUrl } from '@/utils/netease/song'
+import { kv } from '@vercel/kv'
 
 export const GET: APIRoute = async ({ params, request }) => {
   const { id } = params
@@ -12,8 +13,21 @@ export const GET: APIRoute = async ({ params, request }) => {
     })
   }
 
+  const cachedUrl = await kv.get(`song-url-${id}`)
+  if (cachedUrl) {
+    return new Response(JSON.stringify({ url: cachedUrl }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+
   const cookie = request.headers.get('cookie')
   const data = await getSongUrl(id, cookie || undefined)
+
+  if (data.url)
+    await kv.set(`song-url-${id}`, data.url, { ex: 21600 })
 
   return new Response(JSON.stringify(data), {
     status: 200,
